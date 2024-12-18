9f56e580ccdb7290388ec9f0b010e49928ef54ce
Leena Gupta <leenaguptalg2@gmail.com>
2024-12-18 18:48:25 +0530

    instruction-try

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import User from "./pages/User";
 import AdminPage from "./pages/Admin";
+import Auto from "./pages/Auto";
 const App = () => {
   return (
     <Router>
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/" element={<Home />} /> {/* Home component */}
         <Route path="/admin" element={<AdminPage />} />{" "}
         <Route path="/map" element={<User />} /> {/* User component */}
+        <Route path="/auto" element={<Auto />} /> {/* Auto component */}
         {/* AdminPage component */}
       </Routes>
     </Router>
